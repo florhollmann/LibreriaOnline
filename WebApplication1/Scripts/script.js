130703dc384d5ad7c93bfd85f838a580e59d4be8
Flor Hollmann <Florh89@hotmail.com>
2019-07-22 22:21:59 -0300

    se modifico el MODEL data base, se agregaron controladores de autores y libros se agrego un motor de busqueda dentro de los libros y se agrego data a la DB LIBRERIA

---
+++ WebApplication1/Scripts/script.js
@@ -0,0 +1,33 @@
+﻿var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
+
+/*Funcion BUSCAR*/
+function buscar() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
