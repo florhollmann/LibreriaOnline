896d84255b9a2c417c944ceefb24aba610c4d46a
Flor Hollmann <florh89@hotmail.com>
2019-07-24 16:44:25 -0300

    Agregue Inspieration controller, contact us y links directos a CREATE perfil

---
+++ WebApplication1/Controllers/Inspiration_ProfilesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Inspiration_ProfilesController : Controller
+    {
+        private LibreriaDBEntities db = new LibreriaDBEntities();
+
+        // GET: Inspiration_Profiles
+        public ActionResult Index()
+        {
+            return View(db.Inspiration_Profiles.ToList());
+        }
+
+        // GET: Inspiration_Profiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inspiration_Profiles inspiration_Profiles = db.Inspiration_Profiles.Find(id);
+            if (inspiration_Profiles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inspiration_Profiles);
+        }
+
+        // GET: Inspiration_Profiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inspiration_Profiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre,email")] Inspiration_Profiles inspiration_Profiles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inspiration_Profiles.Add(inspiration_Profiles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inspiration_Profiles);
+        }
+
+        // GET: Inspiration_Profiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inspiration_Profiles inspiration_Profiles = db.Inspiration_Profiles.Find(id);
+            if (inspiration_Profiles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inspiration_Profiles);
+        }
+
+        // POST: Inspiration_Profiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,email")] Inspiration_Profiles inspiration_Profiles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inspiration_Profiles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inspiration_Profiles);
+        }
+
+        // GET: Inspiration_Profiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inspiration_Profiles inspiration_Profiles = db.Inspiration_Profiles.Find(id);
+            if (inspiration_Profiles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inspiration_Profiles);
+        }
+
+        // POST: Inspiration_Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inspiration_Profiles inspiration_Profiles = db.Inspiration_Profiles.Find(id);
+            db.Inspiration_Profiles.Remove(inspiration_Profiles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+       
+    }
+}
--- WebApplication1/Scripts/script.js
+++ WebApplication1/Scripts/script.js
@@ -1,6 +1,8 @@
 ﻿var slideIndex = 0;
 showSlides();
 
+/* -------------------FUNCION SLIDES CAROUSEL ======-----*/
+
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -13,7 +15,7 @@ function showSlides() {
     setTimeout(showSlides, 2000);
 }
 
-/*Funcion BUSCAR*/
+/*Funcion --------------------BUSCAR POR TECLADO ------------------------------*/
 function buscar() {
     var input, filter, table, tr, td, i;
     input = document.getElementById("myInput");
@@ -31,3 +33,48 @@ function buscar() {
         }
     }
 }
+
+/*-----------------POP UP*-------=----------------------*/
+
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function () {
+        $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function () {
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function () {
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
+function clickDelA(e) {
+    e.preventDefault()
+
+    let div = document.createElement("div")
+    let p = document.createElement("p")
+    let aceptar = document.createElement("button")
+    let cancelar = document.createElement("button")
+
+    //div.className = "popup"
+    div.classList.add("popup")
+    //Nodo.classList.add()
+    //Nodo.classList.remove()
+    //Nodo.classList.toggle()
+
+
+    p.innerText = "Esta seguro que quiere salir de esta pagina?"
+    aceptar.innerText = "Aceptar"
+    aceptar.addEventListener("click", redirigir)
+    //aceptar.id = "aceptar"
+    cancelar.innerText = "Cancelar"
+    cancelar.addEventListener("click", borrarElemento)
+    //cancelar.id = "cancelar"
+
+    div.appendChild(p)
+    div.appendChild(aceptar)
+    div.appendChild(cancelar)
+
+    document.body.appendChild(div)
+    //Event.preventDefault() : Detiene el comportamiento por default de cualquier nodo
+    console.log("click del a!")
